13191dbcf9f1c1301f0cd0b188de7fd3251cc976
Shivam <alvindevil568@gmail.com>
2025-10-12 17:01:30 +0530

    resumeUpt

--- src/app/pages/landingPage.tsx
+++ src/app/pages/landingPage.tsx
@@ -71,7 +71,7 @@ const LandingPage = () => {
           title: "Tech Entrepreneur",
           quote:
             "WHETHER IT’S WRITING CODE OR STRUCTURING A LIFE, I AIM FOR CLARITY, CALM AND LONG-TERM IMPACT. I BELIEVE GOOD SYSTEMS ARE BUILT WITH INTENT AND CONSISTENCY",
-          cvLink: "https://drive.google.com/uc?export=download&id=1H9I2-6ff58Rc-iU1ylDrHwHILV1rhUY- ",
+          cvLink: "https://drive.google.com/uc?export=download&id=1YD0fkbzVVyJ3g8ZoCYDFKDJJF_ZKZm75 ",
           contactLink:
             "https://mail.google.com/mail/?view=cm&fs=1&to=yadavs47334@gmail.com",
           cta:{
